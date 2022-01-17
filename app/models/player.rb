79a9c73a5e362edc1f4715f06c2cbaf1ffd0fdaa
dajhely angles <assiaxramx10@gmail.com>
2022-01-17 08:08:28 -0500

    Adding filters per status and ranking

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,16 @@
 class Player < ApplicationRecord
   validates :nickname, :avatar, :ranking, :status, presence: true
+
+  STATUS = [
+    ['Todos',nil],
+    ['Oro','oro'],
+    ['Plata','plata'],
+    ['Bronce','bronce'],
+  ]
+
+  RANKING = [
+    ['Todos',nil],
+    ['Mayor puntaje','desc'],
+    ['Menor puntaje','asc'],
+  ]
 end
