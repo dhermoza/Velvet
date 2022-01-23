99b2ce49438fed1e58e52e25d678c43c46f929eb
Denise Hermoza <denise.hermoza@pucp.pe>
2022-01-23 17:21:18 -0500

    fix ordering

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,9 +2,8 @@ class Player < ApplicationRecord
   # include Filterable
   # self.per_page = 20
   validates :nickname, :avatar, :ranking, :status, presence: true
-  # scope :in_print, -> { where(out_of_print: false) }
-  scope :filter_by_status, ->(status) { where(status: status) }
-  # scope :filter_by_status, ->(status) { where status: status }
+
+  scope :filter_by_status, ->(status) { where status }
   scope :order_per_ranking, ->(ranking) { order ranking }
   # scope :desc, order("players.player_at DESC")
 
